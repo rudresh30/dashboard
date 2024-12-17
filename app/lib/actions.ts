89268d37fa3149c438b2689050ee21425951ffa2
rudresh30 <rudresh.pandya@gmail.com>
2024-12-17 22:23:15 +0530

    add error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,8 +26,16 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`INSERT INTO invoices(customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    try {
+        await sql`INSERT INTO invoices(customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;    
+    } catch (error) {
+        console.error(`error in creating new invoice - ${error}`);
+        
+        throw new Error('could not create invoice - please check all parameters and try again');
+    }
+
+    
 
     revalidatePath('/dashboard/invoices')
 
@@ -44,13 +52,26 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`UPDATE invoices SET customer_id=${customerId}, amount=${amountInCents}, status=${status} WHERE id=${id}`;
+    try {
+        await sql`UPDATE invoices SET customer_id=${customerId}, amount=${amountInCents}, status=${status} WHERE id=${id}`;
+        
+    } catch (error) {
+        console.error(`could not update the invoice - ${error}`);        
+        throw Error('could not update invoice');
+    }
+
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices where id=${id}`;
+
+    try {
+        await sql`DELETE FROM invoices where id=${id}`;
+    
+    } catch (error) {
+        console.error(`could not delete the invoice = ${error}`);
+    }
     revalidatePath('/dashboard/invoices');
 }
